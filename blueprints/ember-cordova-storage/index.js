610a5559b08f3e8f06550bc2f09dab90ad114af8
Sven <Svenluecke48@gmx.de>
2016-11-23 11:23:10 +0100

    install cordova-plugin-nativestorage

---
+++ blueprints/ember-cordova-storage/index.js
@@ -0,0 +1,19 @@
+/*jshint node:true*/
+var awaitCommand = require('../../lib/await-command');
+var VersionChecker = require('ember-cli-version-checker');
+
+module.exports = {
+  description: 'Installs the cordova nativeStorage plugin',
+
+  normalizeEntityName: function() {},
+
+  afterInstall: function() {
+    var checker = new VersionChecker(this);
+    var dep = checker.for('ember-cordova', 'npm');
+    if (!dep.version) {
+      throw new Error("ember-cordova-storage requires ember-cordova.");
+    }
+
+    return awaitCommand('ember cdv plugin add cordova-plugin-nativestorage --save');
+  }
+};
